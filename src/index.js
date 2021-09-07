beb0e936f9eba6d50b4724b668c0b2463feaaa7d
Deepakdanger <depk.96.kumar@gmail.com>
2021-09-07 15:47:06 +0530

    Add bird

--- src/index.js
+++ src/index.js
@@ -10,6 +10,59 @@ let sprite = new Image();
 sprite.src = "src/assets/images/sprite.png";
 console.log(sprite);
 
+//game page
+const state={
+    current:0,
+    getReady:0,
+    game:1,
+    gameOver:2
+}
+
+cvs.addEventListener("click",function(){
+    console.log(state.current);
+    switch(state.current){
+        case state.getReady:
+            state.current= state.game;
+            break;
+        case state.game:
+            bird.move();
+            state.current = state.gameOver;
+            break;
+        case state.gameOver:
+            state.current = state.getReady;
+            break;
+    }
+})
+//1st page
+const getReady ={
+    sX:0,
+    sY:228,
+    w:173,
+    h:152,
+    x:cvs.width/2-(173/2),
+    y:200,
+    draw:function(){
+        if(state.current==state.getReady){
+        ctx.drawImage(sprite,this.sX,this.sY,this.w,this.h,this.x,this.y,this.w,this.h);
+       }
+    }
+}
+
+//last page
+const gameOver ={
+    sX:175,
+    sY:228,
+    w:225,
+    h:202,
+    x:cvs.width/2-(225/2),
+    y:200,
+    draw:function(){
+        if(state.current==state.gameOver){
+        ctx.drawImage(sprite,this.sX,this.sY,this.w,this.h,this.x,this.y,this.w,this.h);
+        }
+    }
+}
+
 //setting cloud
 
 let cloud = {
@@ -63,20 +116,22 @@ const bird={
         {sX:276,sY:164},
         {sX:276,sY:139}
     ],
-    x:60,
+    x:70,
     y:150,
     w:34,
     h:26,
     frame:0,
-    period:10,
+    period:8,
     draw:function(){
         let bird = this.animation[this.frame];
         ctx.drawImage(sprite,bird.sX,bird.sY,this.w,this.h,this.x,this.y,this.w,this.h);
-
     },
     update:function(){
-        this.frame = this.frame + frames%this.period == 0 ? 1:0;  //flapping the bird
-        this.frame = this.frame % this.animation.length; //resetting the frame
+        this.frame += frames%this.period==0 ? 1:0;  //flapping the bird
+        this.frame = this.frame%this.animation.length; //resetting the frame
+    },
+    move:function(){
+
     }
 }
 // for drawing
@@ -84,6 +139,8 @@ const draw = () => {
     cloud.draw();
     ground.draw();
     bird.draw();
+    getReady.draw();
+    gameOver.draw();
 };
 
 // for update
