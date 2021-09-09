3ff3cc53d73092b78471751e18f2c5802a7f6f5b
Deepakdanger <depk.96.kumar@gmail.com>
2021-09-09 13:24:19 +0530

    solve linter errors

--- webpack/base.js
+++ webpack/base.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-//const path = require('path');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
