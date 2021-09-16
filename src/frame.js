07a3a27bad312960ec9742c9525ea558295f1504
Deepakdanger <depk.96.kumar@gmail.com>
2021-09-16 15:43:10 +0530

    Add leaderboard after game over

--- src/frame.js
+++ src/frame.js
@@ -4,8 +4,14 @@ cvs.setAttribute('width', '1349');
 document.body.appendChild(cvs);
 const ctx = cvs.getContext('2d');
 
+const form1 = document.getElementById('form1');
+form1.classList.add('removev');
+
 const sprite = new Image();
 sprite.src = '../src/assets/images/sprite.png';
+
+const boardpic = new Image();
+boardpic.src = '../src/assets/images/leaderboard.png';
 
 const music = new Audio();
 music.src = '../src/assets/audio/dreams.mp3';
@@ -18,31 +24,115 @@ swooshing.src = '../src/assets/audio/swooshing.wav';
 
 const state = {
   current: 0,
-  getReady: 0,
-  game: 1,
-  gameOver: 2,
+  formName: 0,
+  getReady: 1,
+  game: 2,
+  gameOver: 3,
+  board: 4,
 };
 
 const score = {
   bestscore: (parseFloat(localStorage.getItem('bestscore')) || 0),
   value: 0,
+  sX: 0,
+  sY: 0,
+  w: 536,
+  h: 464,
+  x: cvs.width / 2 - (537 / 2),
+  y: 50,
+  dataArray: {},
+  nameX: cvs.width / 2 - 70,
+  scoreX: cvs.width / 2 + 150,
+  scoreY: 290,
+  username: 'greyer',
   draw() {
-    ctx.fillStyle = '#000000';
     if (state.current === state.game) {
+      ctx.fillStyle = '#000000';
       ctx.font = '40px teko';
       ctx.fillText(this.value, 100, 80);
-    } else if (state.current === state.gameOver) {
-      ctx.font = '25px teko';
-      ctx.fillText(this.value, cvs.width / 2 + 65, 300);
-
-      ctx.font = '25px teko';
-      ctx.fillText(this.bestscore, cvs.width / 2 + 65, 340);
+    } else if (state.current === state.board) {
+      ctx.drawImage(boardpic, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+      ctx.fillStyle = '#ffffff';
+      ctx.font = '30px teko';
+      ctx.fillText(this.value, cvs.width / 2 - 10, 185);
+      ctx.fillStyle = '#000000';
+      for (let i = this.dataArray.result.length - 1; i > this.dataArray.result.length - 6; i -= 1) {
+        ctx.fillText(this.dataArray.result[i].user, this.nameX, this.scoreY);
+        ctx.fillText(this.dataArray.result[i].score, this.scoreX, this.scoreY);
+        this.scoreY += 50;
+      }
+      this.scoreY = 290;
     }
+  },
+  draw1(data) {
+    console.log(data);
+    this.dataArray = data;
+  },
+  update() {
+    const addData = { user: score.username, score: score.value };
+    this.dataArray.result.push(addData);
+    for (let i = 0; i < this.dataArray.result.length; i += 1) {
+      // Last i elements are already in place
+      for (let j = 0; j < (this.dataArray.result.length - i - 1); j += 1) {
+        // Checking if the item at present iteration
+        // is greater than the next iteration
+        if (this.dataArray.result[j].score > this.dataArray.result[j + 1].score) {
+          // If the condition is true then swap them
+          const temp = this.dataArray.result[j];
+          this.dataArray.result[j] = this.dataArray.result[j + 1];
+          this.dataArray.result[j + 1] = temp;
+        }
+      }
+    }
+    console.log(this.dataArray);
   },
   reset() {
     this.value = 0;
   },
 
+};
+
+const leaderboard = {
+  key: '3EzsYnpgOQKdZOndzcO5',
+  url: 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/3EzsYnpgOQKdZOndzcO5/scores/',
+  post(get = true, data) {
+    return fetch(this.url, {
+      method: get ? 'GET' : 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify(data),
+    })
+      .then((data) => data.json());
+  },
+  postScore() {
+    const data = { user: score.username, score: score.value };
+    this.post(false, data)
+      .then(() => console.log('succesfully send'))
+      .catch((e) => console.log(e.message));
+  },
+  getScore() {
+    this.post()
+      .then((data) => score.draw1(data))
+      .catch((e) => console.log(e.message));
+  },
+
+};
+
+const formName = {
+  draw() {
+    console.log('hello');
+    if (state.current === state.formName) {
+      form1.classList.remove('removev');
+      form1.classList.add('addv');
+      console.log(state.current);
+    }
+  },
 };
 
 const getReady = {
@@ -55,6 +145,7 @@ const getReady = {
   draw() {
     if (state.current === state.getReady) {
       ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+      ctx.fillStyle = '#000000';
     }
   },
 };
@@ -64,9 +155,9 @@ const gameOver = {
   sX: 175,
   sY: 228,
   w: 225,
-  h: 202,
+  h: 48,
   x: cvs.width / 2 - (225 / 2),
-  y: 200,
+  y: 240,
   draw() {
     if (state.current === state.gameOver) {
       ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
@@ -76,4 +167,5 @@ const gameOver = {
 
 export {
   state, score, getReady, gameOver, music, sprite, cvs, ctx, hit, swooshing,
+  leaderboard, form1, formName,
 };
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import bird from './bird';
 import { cloud, ground } from './ground';
 import { alien, ball } from './alien';
 import {
-  state, score, getReady, gameOver, music, cvs, ctx, swooshing,
+  state, score, getReady, gameOver, music, cvs, ctx, swooshing, leaderboard, form1, formName,
 } from './frame';
 import pipes from './pipes';
 
@@ -11,6 +11,8 @@ let frames = 0;
 
 cvs.addEventListener('click', () => {
   switch (state.current) {
+    case state.formName:
+      break;
     case state.getReady:
       if (state.current === state.getReady) {
         music.play();
@@ -18,16 +20,22 @@ cvs.addEventListener('click', () => {
       }
       state.current = state.game;
       swooshing.play();
+      leaderboard.getScore();
       break;
     case state.game:
       bird.move();
       break;
     case state.gameOver:
-      state.current = state.getReady;
+      state.current = state.board;
       music.load();
       pipes.reset();
       ball.reset();
+      score.update();
+      break;
+    case state.board:
+      leaderboard.postScore();
       score.reset();
+      state.current = state.formName;
       break;
     default:
   }
@@ -45,6 +53,7 @@ const draw = () => {
   getReady.draw();
   gameOver.draw();
   score.draw();
+  formName.draw();
 };
 
 const update = () => {
@@ -61,4 +70,12 @@ const loop = () => {
   frames += 1;
   requestAnimationFrame(loop);
 };
+
+form1.addEventListener('submit', (e) => {
+  e.preventDefault();
+  state.current = state.getReady;
+  form1.classList.remove('addv');
+  form1.classList.add('removev');
+});
+
 loop();
